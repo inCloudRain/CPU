12f3b39da1f10b43fa8c76a5170f2fab18cd1e5e
inCloudRain <icr.6@outlook.com>
2025-11-28 21:17:31 +0800

    实现所有R型指令及I型计算指令

--- ID.v
+++ ID.v
@@ -152,11 +152,101 @@ module ID(
         case(opcode)
             6'b000000: begin
             case(func)  //R型计算指令
+                6'b100000: begin   //add
+                    op_add = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b100001: begin   //addu
+                    op_add = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b100010: begin   //sub
+                    op_sub = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
                 6'b100011: begin   //subu
                     op_sub = 1'b1;
-                    sel_alu_src1[0] = 1'b1; //rs
-                    sel_alu_src2[0] = 1'b1; //rt
-                    sel_rf_dst[0] = 1'b1; //rd
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b100100: begin   //and
+                    op_and = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b100101: begin   //or
+                    op_or = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b100110: begin   //xor
+                    op_xor = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b100111: begin   //nor
+                    op_nor = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b101010: begin   //slt
+                    op_slt = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b101011: begin   //sltu
+                    op_sltu = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b000000: begin   //sll
+                    op_sll = 1'b1;
+                    sel_alu_src1[2] = 1'b1; // sa_zero_extend
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b000010: begin   //srl
+                    op_srl = 1'b1;
+                    sel_alu_src1[2] = 1'b1; // sa_zero_extend
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b000011: begin   //sra
+                    op_sra = 1'b1;
+                    sel_alu_src1[2] = 1'b1; // sa_zero_extend
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b000100: begin   //sllv
+                    op_sll = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b000110: begin   //srlv
+                    op_srl = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
+                end
+                6'b000111: begin   //srav
+                    op_sra = 1'b1;
+                    sel_alu_src1[0] = 1'b1; // rs
+                    sel_alu_src2[0] = 1'b1; // rt
+                    sel_rf_dst[0] = 1'b1;   // rd
                 end
                 //R型跳转指令
                 6'b001000: begin  //jr
@@ -164,24 +254,67 @@ module ID(
                     br_e = 1'b1;
                     br_addr = data1;
                 end
+                6'b001001: begin  //jalr
+                    rf_we = 1'b1;
+                    op_add = 1'b1;
+                    sel_alu_src1[1] = 1'b1; // pc
+                    sel_alu_src2[2] = 1'b1; // 32'd8
+                    sel_rf_dst[0] = 1'b1;   // rd
+                    br_e = 1'b1;
+                    br_addr = data1;
+                end
+                default: begin
+                    // 未定义R型func，全部无效
+                    rf_we = 1'b0;
+                end
             endcase end
-            //I型计算指令
-            6'b001101: begin   //ori
-                op_or = 1'b1;
-                sel_alu_src1[0] = 1'b1; //rs
-                sel_alu_src2[3] = 1'b1; //imm_zero_extend
-                sel_rf_dst[1] = 1'b1; //rt
-            end
-            6'b001111: begin   //lui
-                op_lui = 1'b1;
-                sel_alu_src2[1] = 1'b1; //imm_sign_extend
-                sel_rf_dst[1] = 1'b1; //rt
-            end
-            6'b001001: begin   //addiu
+            // I型计算指令
+            6'b001000: begin   // addi
                 op_add = 1'b1;
-                sel_alu_src1[0] = 1'b1; //rs
-                sel_alu_src2[1] = 1'b1; //imm_sign_extend
-                sel_rf_dst[1] = 1'b1; //rt
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[1] = 1'b1; // imm_sign_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001001: begin   // addiu
+                op_add = 1'b1;
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[1] = 1'b1; // imm_sign_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001100: begin   // andi
+                op_and = 1'b1;
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[3] = 1'b1; // imm_zero_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001101: begin   // ori
+                op_or = 1'b1;
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[3] = 1'b1; // imm_zero_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001110: begin   // xori
+                op_xor = 1'b1;
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[3] = 1'b1; // imm_zero_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001111: begin   // lui
+                op_lui = 1'b1;
+                sel_alu_src2[1] = 1'b1; // imm_sign_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001010: begin   // slti
+                op_slt = 1'b1;
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[1] = 1'b1; // imm_sign_extend
+                sel_rf_dst[1] = 1'b1;   // rt
+            end
+            6'b001011: begin   // sltiu
+                op_sltu = 1'b1;
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[1] = 1'b1; // imm_sign_extend
+                sel_rf_dst[1] = 1'b1;   // rt
             end
         default: begin
             //跳转指令初始化
@@ -197,9 +330,9 @@ module ID(
             6'b000011: begin  //jal
                 rf_we = 1'b1;
                 op_add = 1'b1;
-                sel_alu_src1[1] = 1'b1; //pc
-                sel_alu_src2[2] = 1'b1; //32'b8
-                sel_rf_dst[2] = 1'b1; //$31
+                sel_alu_src1[1] = 1'b1; // pc
+                sel_alu_src2[2] = 1'b1; // 32'b8
+                sel_rf_dst[2] = 1'b1;   // $31
                 br_addr = {id_pc[31:28], instr_index, 2'b00};
             end
             6'b000100: begin  //beq
@@ -219,8 +352,8 @@ module ID(
             6'b101011: begin  //sw
                 data_ram_wen = 4'b1111;
                 op_add = 1'b1;
-                sel_alu_src1[0] = 1'b1; //rs
-                sel_alu_src2[1] = 1'b1; //imm_sign_extend
+                sel_alu_src1[0] = 1'b1; // rs
+                sel_alu_src2[1] = 1'b1; // imm_sign_extend
             end
         endcase end endcase end endcase
     end
@@ -232,7 +365,7 @@ module ID(
     // sel for regfile address
     assign rf_waddr = {5{sel_rf_dst[0]}} & rd 
                     | {5{sel_rf_dst[1]}} & rt
-                    | {5{sel_rf_dst[2]}} & 32'd31;
+                    | {5{sel_rf_dst[2]}} & 5'd31;
 
 
     assign id_to_ex_bus = {
